83c1750f0cd0c83ee38c6f22bd0a16c8b0f2dc3f
Mattia Massarenti <mattia.mas2003@gmail.com>
2025-08-16 16:42:33 +0200

    set environment variables from launch script

--- backend/dev.sh
+++ backend/dev.sh
@@ -1,3 +1,8 @@
 export CORS_ALLOW_ORIGIN="http://localhost:5173"
+export OAUTH_CLIENT_ID="infra_mllm"
+export OAUTH_CLIENT_SECRET="I0P0yo66HtWJUyRl8l66zCTmQrtwSSRq"
+export OPENID_PROVIDER_URL="https://ailb-web.ing.unimore.it/sso/realms/aimagelab-srv/.well-known/openid-configuration"
+export OPENID_REDIRECT_URI="/oauth/oidc/callback"
+
 PORT="${PORT:-8080}"
 uvicorn open_webui.main:app --port $PORT --host 0.0.0.0 --forwarded-allow-ips '*' --reload
